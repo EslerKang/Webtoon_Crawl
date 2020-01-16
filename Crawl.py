50d8486f9d2a9634c9d17b90214412f6f9d32c8c
eslerkang <nexus3131@naver.com>
2020-01-16 21:41:21 +0900

    파이썬, 서버 공부용으로 시작한 네이버 웹툰 크롤러. 현재까지 진행된 사항은 크롤링 후 웹툰 이름의 폴더에 한 화씩 pdf로 저장. 이미 있는 내용 건너뛰기 등. -불법적인 목적의 이용 금함

--- Crawl.py
+++ Crawl.py
@@ -10,6 +10,7 @@ from selenium.webdriver.common.by import By
 from img2pdf import convert
 import time
 import shutil
+from PIL import Image
 
 
 # pdf 파일들 이름 오름차순 정렬 시 키 값
@@ -38,6 +39,8 @@ def crawl_webtoon(episode_url):
     chrome_options.add_argument('lang=ko_KR')
     driver = webdriver.Chrome(driver_path, chrome_options=chrome_options)
     driver.get('https://nid.naver.com/nidlogin.login')
+    driver.implicitly_wait(10)
+    time.sleep(2)
 
     print("Going to Login Page")
 
@@ -45,68 +48,86 @@ def crawl_webtoon(episode_url):
     driver.execute_script("document.getElementsByName('pw')[0].value=\'" + PWD + "\'")
     driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
 
-    print("Login success!")
+    print("Login success!\n")
 
-    driver.implicitly_wait(30)
-    time.sleep(2.5)
+    time.sleep(2)
+    try:
+        for url in episode_url:
 
-    for url in episode_url:
+            driver.get(url)
 
-        driver.get(url)
-        # time.sleep(10)
-        try:
-            WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR,"img[src='https://static-comic.pstatic.net/staticImages/COMICWEB/NAVER/img/common/blank.gif']")))
-        except Exception as ex:
-            print("Page Loading Error")
-            print(ex)
+            try:
+                WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR,
+                                                                                    "img[src='https://static-comic.pstatic.net/staticImages/COMICWEB/NAVER/img/common/blank.gif']")))
+            except Exception as ex:
+                print("Page Loading Error")
+                print(ex)
 
-        print("Loaded Webtoon Page!")
+            print("Loaded Webtoon Page!")
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
 
-        print('Parse success')
+            print('Parse success')
 
-        comic_title = ' '.join(soup.select('.comicinfo h2')[0].text.split())
-        ep_title = ' '.join(soup.select('.tit_area h3')[0].text.split())
-        if ep_title+".pdf" in os.listdir(os.path.join(os.path.dirname(__file__), comic_title)):
-            print(ep_title+" Already Exist")
-            continue
+            comic_title = ' '.join(soup.select('.comicinfo h2')[0].text.split())
+            ep_title = ' '.join(soup.select('.tit_area h3')[0].text.split())
 
-        image_name_list = []
-        for img_tag in soup.select('.wt_viewer img'):
-            image_file_url = img_tag['src']
-            image_dir_path = os.path.join(os.path.dirname(__file__), comic_title, ep_title+"_img")
-            image_name = os.path.basename(image_file_url)
-            image_name_splited = image_name.split('_')
-            image_file_path = os.path.join(image_dir_path, image_name_splited[len(image_name_splited)-1])
-            image_name_list.append(image_name_splited[len(image_name_splited)-1])
+            if os.path.exists(os.path.join(os.path.dirname(__file__), comic_title)):
+                if ep_title+".pdf" in os.listdir(os.path.join(os.path.dirname(__file__), comic_title)):
+                    print(ep_title+" Already Exist\n")
+                    continue
 
-            if not os.path.exists(image_dir_path):
-                os.makedirs(image_dir_path)
+            image_name_list = []
+            for img_tag in soup.select('.wt_viewer img'):
+                image_file_url = img_tag['src']
+                image_name = os.path.basename(image_file_url)
+                image_dir_path = os.path.join(os.path.dirname(__file__), comic_title, ep_title + "_img")
+                image_name_splited = image_name.split('_')
+                image_file_path = os.path.join(image_dir_path, image_name_splited[len(image_name_splited)-1])
+                image_name_list.append(image_name_splited[len(image_name_splited)-1])
 
-            print(image_file_path)
+                if not os.path.exists(image_dir_path):
+                    os.makedirs(image_dir_path)
 
-            headers = {'Referer': url}
-            image_file_data = requests.get(image_file_url, headers=headers).content
+                print(image_file_path)
 
-            open(image_file_path, 'wb').write(image_file_data)
+                headers = {'Referer': url}
+                image_file_data = requests.get(image_file_url, headers=headers).content
 
-        print('Completed to download!')
+                open(image_file_path, 'wb').write(image_file_data)
 
-        os.chdir(image_dir_path)
+            print('Completed to download!')
 
-        with open(ep_title+".pdf", "wb") as f:
-            f.write(convert([i for i in image_name_list if i.endswith('.jpg')]))
-            f.close()
-        os.chdir("../")
-        shutil.move(image_dir_path+"/"+ep_title+".pdf", ep_title+".pdf")
+            os.chdir(image_dir_path)
+            maximum = 0
+            for image_mod in image_name_list:
+                im = Image.open(image_mod)
+                width, height = im.size
+                if width > maximum:
+                    maximum = width
+                im.close()
+            for image_mod in image_name_list:
+                im = Image.open(image_mod)
+                width, height = im.size
+                image = im.resize((maximum, height))
+                image.save(image_mod)
+                im.close()
+            print("Image resize completed!")
 
-        shutil.rmtree(image_dir_path)
-        os.chdir("../")
+            with open(ep_title+".pdf", "wb") as f:
+                f.write(convert([i for i in image_name_list if i.endswith('.jpg')]))
+                f.close()
+            os.chdir("../")
+            shutil.move(image_dir_path+"/"+ep_title+".pdf", ep_title+".pdf")
 
-        print("Merge complete!\n")
+            shutil.rmtree(image_dir_path)
+            os.chdir("../")
 
+            print("Merge complete!\n")
+    except Exception as ex:
+        print(ex)
+        print("Error!!")
     driver.quit()
 
     print("Complete!")
@@ -115,9 +136,11 @@ def crawl_webtoon(episode_url):
 if __name__ == '__main__':
     episode_url = []
     webtoon = {
-        '670143': 208
+        # '293523': 131
+        '655749': 56
     }
     for i in webtoon.keys():
-        for j in range(206, webtoon[i]+1):
+        for j in range(1, webtoon[i]+1):
             episode_url.append('https://comic.naver.com/webtoon/detail.nhn?titleId='+i+'&no='+str(j))
+    print("Crawling information added")
     crawl_webtoon(episode_url)
